7a262e25c3d2544ea6a3c871ff5695ecb005cb9d
Yanice <git@yanice.me>
2026-07-17 14:28:29 +0200

    fix: correct file name on win + icon on win

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
 fn main() {
     slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("icons/icon.ico");
+        res.compile().unwrap();
+    }
 }
